69886160f9e68ad202f2120a33678bca07456265
yomarhub <omar.yassine@ynov.com>
2025-09-24 23:53:34 +0200

    Add "Stack Icons" feature and refactor icon generation
    Enhanced the `IconCreator` plugin for Paint.NET with a new
    "Stack Icons" feature, allowing users to include smaller icon
    sizes when saving multi-resolution icons. Updated the save
    dialog UI to support this feature.
    
    Refactored the `OnSaveT` method for efficiency and clarity,
    including optimized multi-resolution icon generation logic
    and improved memory management. Removed redundant code.
    
    Updated the `System.Drawing.Common` package to version
    9.0.9. Improved overall code readability and maintainability
    using modern C# features.

--- IconFileType.cs
+++ IconFileType.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using PaintDotNet;
+using PaintDotNet.IndirectUI;
 using PaintDotNet.PropertySystem;
 using PaintDotNet.Rendering;
 
@@ -26,6 +27,7 @@ namespace IconCreatorFileType
     {
 
         public const string ImageSizeString = "Image Size";
+        public const string LessAndEqualSizesString = "Stack Icons";
         public const string SourceCodeString = "Source Code";
         public const string WebSiteLinkValue = @"https://github.com/KyleTTucker/Paint.net-Icon-Creator";
 
@@ -43,39 +45,42 @@ namespace IconCreatorFileType
         {
             List<Property> props =
             [
-                StaticListChoiceProperty.CreateForEnum(ImageSizeString, ImageSize.Icon_Auto),
+                StaticListChoiceProperty.CreateForEnum(ImageSizeString, ImageSize.Icon_256x256),
+                new BooleanProperty(LessAndEqualSizesString), // Property.Create(typeof(bool), LessAndEqualSizesString, true),
                 new UriProperty(SourceCodeString, new Uri(WebSiteLinkValue))
             ];
 
             return new PropertyCollection(props);
         }
 
+        public override ControlInfo OnCreateSaveConfigUI(PropertyCollection props)
+        {
+            ControlInfo configUI = CreateDefaultSaveConfigUI(props);
+            configUI.SetPropertyControlValue(LessAndEqualSizesString, ControlInfoPropertyNames.DisplayName, string.Empty); // Hide the label
+            configUI.SetPropertyControlValue(LessAndEqualSizesString, ControlInfoPropertyNames.Description, "Add all the smaller sizes to the icons"); // Set the description
+
+            return configUI;
+        }
+
         protected override Document OnLoad(Stream input)
         {
-            Icon NewIcon = new(input);
-            Bitmap bitmapOfIcon = NewIcon.ToBitmap();
-
+            using Icon icon = new(input);
+            Bitmap bitmap = icon.ToBitmap();
             Document document = null;
-
-            if (bitmapOfIcon.Width > 0 && bitmapOfIcon.Height > 0)
+            if (bitmap.Width > 0 && bitmap.Height > 0)
             {
-                document = new Document(bitmapOfIcon.Width, bitmapOfIcon.Height);
-
-                BitmapLayer layer = Layer.CreateBackgroundLayer(bitmapOfIcon.Width, bitmapOfIcon.Height);
-
+                document = new Document(bitmap.Width, bitmap.Height);
+                BitmapLayer layer = Layer.CreateBackgroundLayer(bitmap.Width, bitmap.Height);
                 Surface surface = layer.Surface;
-
                 for (int y = 0; y < surface.Height; y++)
                 {
                     for (int x = 0; x < surface.Width; x++)
                     {
-                        surface[x, y] = bitmapOfIcon.GetPixel(x, y);
+                        surface[x, y] = bitmap.GetPixel(x, y);
                     }
                 }
-
                 document.Layers.Add(layer);
             }
-
             return document;
         }
 
@@ -105,6 +110,73 @@ namespace IconCreatorFileType
 
             //Resize image
             ImageSize bitDepth = (ImageSize)token.GetProperty(ImageSizeString).Value;
+            bool stackIcons = (bool)token.GetProperty(LessAndEqualSizesString).Value;
+            if (bitDepth == ImageSize.Icon_All || stackIcons)
+            {
+                // Refactored: efficient generation of a multi-resolution icon
+                int[] sizes = new int[] { 16, 32, 48, 64, 128, 256 };
+
+                // If stacking icons, filter sizes based on selected size
+                if (stackIcons && bitDepth != ImageSize.Icon_All)
+                {
+                    // Only include sizes less than or equal to the selected size
+                    int maxSize = bitDepth switch
+                    {
+                        ImageSize.Icon_16x16 => 16,
+                        ImageSize.Icon_32x32 => 32,
+                        ImageSize.Icon_48x48 => 48,
+                        ImageSize.Icon_64x64 => 64,
+                        ImageSize.Icon_128x128 => 128,
+                        ImageSize.Icon_256x256 => 256,
+                        _ => 32,
+                    };
+                    sizes = Array.FindAll(sizes, sz => sz <= maxSize);
+                }
+
+                var pngImages = new List<byte[]>(sizes.Length);
+                using (var _iconWriter = new BinaryWriter(output, System.Text.Encoding.Default, true))
+                {
+                    // Generate the bitmaps and PNGs in memory
+                    foreach (int sz in sizes)
+                    {
+                        using (var bmp = new Bitmap(ApplyPixels, sz, sz))
+                        using (var ms = new MemoryStream())
+                        {
+                            bmp.Save(ms, ImageFormat.Png);
+                            pngImages.Add(ms.ToArray());
+                        }
+                    }
+
+                    // ICO header
+                    _iconWriter.Write((short)0); // reserved
+                    _iconWriter.Write((short)1); // type: icon
+                    _iconWriter.Write((short)sizes.Length); // number of images
+
+                    int offset = 6 + (16 * sizes.Length); // 6 bytes header + 16 bytes per entry
+                    for (int i = 0; i < sizes.Length; i++)
+                    {
+                        int sz = sizes[i];
+                        byte width = (byte)(sz == 256 ? 0 : sz); // 0 = 256px
+                        byte height = (byte)(sz == 256 ? 0 : sz); // 0 = 256px
+                        _iconWriter.Write(width); // width
+                        _iconWriter.Write(height); // height
+                        _iconWriter.Write((byte)0); // colors
+                        _iconWriter.Write((byte)0); // reserved
+                        _iconWriter.Write((short)0); // color planes
+                        _iconWriter.Write((short)32); // bits per pixel
+                        _iconWriter.Write(pngImages[i].Length); // image size
+                        _iconWriter.Write(offset); // offset
+                        offset += pngImages[i].Length;
+                    }
+                    // PNG data
+                    for (int i = 0; i < pngImages.Count; i++)
+                    {
+                        _iconWriter.Write(pngImages[i]);
+                    }
+                    _iconWriter.Flush();
+                }
+                return;
+            }
             switch (bitDepth)
             {
                 case ImageSize.Icon_Auto:
@@ -127,51 +199,7 @@ namespace IconCreatorFileType
                     ApplyPixels = new Bitmap(ApplyPixels, 256, 256);
                     break;
                 case ImageSize.Icon_All:
-                    // Refactored: efficient generation of a multi-resolution icon
-                    int[] sizes = new int[] { 16, 32, 48, 64, 128, 256 };
-                    var pngImages = new List<byte[]>(sizes.Length);
-                    using (var _iconWriter = new BinaryWriter(output, System.Text.Encoding.Default, true))
-                    {
-                        // Generate the bitmaps and PNGs in memory
-                        foreach (int sz in sizes)
-                        {
-                            using (var bmp = new Bitmap(ApplyPixels, sz, sz))
-                            using (var ms = new MemoryStream())
-                            {
-                                bmp.Save(ms, ImageFormat.Png);
-                                pngImages.Add(ms.ToArray());
-                            }
-                        }
 
-                        // ICO header
-                        _iconWriter.Write((short)0); // reserved
-                        _iconWriter.Write((short)1); // type: icon
-                        _iconWriter.Write((short)sizes.Length); // number of images
-
-                        int offset = 6 + (16 * sizes.Length); // 6 bytes header + 16 bytes per entry
-                        for (int i = 0; i < sizes.Length; i++)
-                        {
-                            int sz = sizes[i];
-                            byte width = (byte)(sz == 256 ? 0 : sz); // 0 = 256px
-                            byte height = (byte)(sz == 256 ? 0 : sz); // 0 = 256px
-                            _iconWriter.Write(width); // width
-                            _iconWriter.Write(height); // height
-                            _iconWriter.Write((byte)0); // colors
-                            _iconWriter.Write((byte)0); // reserved
-                            _iconWriter.Write((short)0); // color planes
-                            _iconWriter.Write((short)32); // bits per pixel
-                            _iconWriter.Write(pngImages[i].Length); // image size
-                            _iconWriter.Write(offset); // offset
-                            offset += pngImages[i].Length;
-                        }
-                        // PNG data
-                        for (int i = 0; i < pngImages.Count; i++)
-                        {
-                            _iconWriter.Write(pngImages[i]);
-                        }
-                        _iconWriter.Flush();
-                    }
-                    return;
                 default:
                     ApplyPixels = new Bitmap(ApplyPixels, 32, 32);
                     break;
